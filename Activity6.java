2c669c3329d7b937f27ba3b991eab24ba6faa2c4
BEN-127-25 <benedictjohnaventajado@gmail.com>
2025-08-16 21:01:34 +0800

    activity 6

--- Activity6.java
+++ Activity6.java
@@ -1,22 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package activity6;
 
-/**
- *
- * @author benedict
- */
-public class Activity6 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Activity6{
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Distance (km): ");
+        double distance = scanner.nextDouble();
+
+        System.out.println("Select class: ");
+        System.out.println("1: Economy class");
+        System.out.println("2: Business class");
+        System.out.print("Class: ");
+        int travelClass = scanner.nextInt();
+
+        double baseFarePerKm = 0;
+
+        if (travelClass == 1) {
+            baseFarePerKm = 250;
+        } else if (travelClass == 2) {
+            baseFarePerKm = 500;
+        } else {
+            System.out.println("Invalid class");
+            scanner.close();
+            return;
+        }
+
+        double totalFare = distance * baseFarePerKm;
+
+        if (distance > 1000) {
+            totalFare = totalFare * 0.9; // 10% discount
+        }
+
+        System.out.printf("Total fare: %.2f\n", totalFare);
+
+        scanner.close();
     }
-    
 }
